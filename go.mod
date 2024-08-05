b4f4a65e79232c518a8b9141d72150f4e2c55e60
0x4f53 <71916237+0x4f53@users.noreply.github.com>
2024-08-05 14:51:40 +0530

    added code

---
+++ go.mod
@@ -0,0 +1,10 @@
+module textsubs
+
+go 1.22.3
+
+require github.com/weppos/publicsuffix-go v0.40.2
+
+require (
+	golang.org/x/net v0.27.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"regexp"
+	"strings"
+
+	"github.com/weppos/publicsuffix-go/publicsuffix"
+)
+
+func removeDuplicates(items []string) []string {
+	encountered := make(map[string]bool)
+	result := []string{}
+
+	for _, item := range items {
+		if !encountered[item] {
+			encountered[item] = true
+			result = append(result, item)
+		}
+	}
+
+	return result
+}
+
+func Parse(text string, unique bool, subdomainsOnly bool) []string {
+
+	//check if item contains domain-legal characters
+	matches := regexp.MustCompile(`([a-zA-Z0-9.-]+)`).FindAllStringSubmatch(text, -1)
+
+	var subdomains []string
+
+	for _, match := range matches {
+		item := match[1]
+
+		item = strings.TrimSpace(item)
+		item = strings.Trim(item, "\n\b\t\a")
+		item = strings.Replace(item, "\n", " ", -1)
+
+		if strings.Contains(item, ".") && // Check if item contains dots at all
+			!strings.Contains(item, "..") && // Check if item contains consecutive dots
+			!strings.HasSuffix(item, ".") && // Domain cannot end in '.'
+			(regexp.MustCompile(`\.[a-zA-Z]{2,}`).MatchString(item)) && // At least 2 characters must exist after dot
+			(regexp.MustCompile(`[a-zA-Z]{1,}\.`).MatchString(item)) && // At least 1 character must exist before dot
+			regexp.MustCompile(`[a-zA-Z]`).MatchString(item) { // At least one letter must exist
+
+			domain, _ := publicsuffix.DomainFromListWithOptions( // Check if TLD is valid
+				publicsuffix.DefaultList,
+				item,
+				&publicsuffix.FindOptions{
+					IgnorePrivate: true,
+				},
+			)
+
+			if len(domain) == 0 {
+				continue
+			}
+
+			if subdomainsOnly && strings.EqualFold(domain, item) { // do not let domains be logged
+				continue
+			}
+
+			subdomains = append(subdomains, item)
+
+		}
+
+	}
+
+	if unique {
+		subdomains = removeDuplicates(subdomains)
+	}
+
+	return subdomains
+}
+
+func main() {
+	data, _ := ioutil.ReadFile("testcase.txt")
+	output := Parse(string(data), true, true)
+	fmt.Println(output)
+}
